df879d4bcf1f614ea04f50b03e7b41fa4e1cb3c2
DINESH KUMAR <dineshkumargvco@gmail.com>
2026-09-09 12:49:12 +0800

    Add portfolio favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+     
+    
   );
 }
